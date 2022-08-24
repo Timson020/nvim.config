a25d266c89963d131896248696255ba834641646
Timson020 <a_sa_f@126.com>
2022-08-24 09:56:00 +0800

    add new language to treesitter

--- plug.config/nvim-treesitter.lua
+++ plug.config/nvim-treesitter.lua
@@ -8,7 +8,7 @@ end
 treesitter.setup({
 	-- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = { "html", "css", "scss", "sql", "solidity", "dart", "gitignore", "json", "vim", "vue", "c", "lua", "go", "python", "dockerfile" },
+  ensure_installed = { "html", "css", "scss", "sql", "solidity", "dart", "gitignore", "json", "vim", "vue", "c", "lua", "go", "typescript", "python", "dockerfile" },
 
   -- 启用代码高亮模块
   highlight = {
