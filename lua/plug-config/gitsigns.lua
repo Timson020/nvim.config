b46f50d7afa57568c9945ddd6c0a1300f935841a
Timson020 <a_sa_f@126.com>
2022-11-15 23:58:34 +0800

    feat: move to lua: step one

---
+++ lua/plug-config/gitsigns.lua
@@ -0,0 +1,23 @@
+local status, gitsigns = pcall(require, "gitsigns")
+if not status then
+  vim.notify("没有找到 gitsigns")
+  return
+end
+
+local config = {
+	signs = {
+		add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+    change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+    delete       = { hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+    untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+  },
+	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>'
+}
+
+gitsigns.setup(config)
---
+++ lua/plug-config/rainbow.lua
@@ -0,0 +1,6 @@
+vim.g.rainbow_active = 1
+
+vim.g.rainbow_conf = {
+	guifgs = { 'royalblue3', 'darkorange3', 'seagreen3', 'firebrick' },
+	-- parentheses = { 'start=/{/ end=/}/ fold', 'start=/\[/ end=/\]/ fold', 'start=/(/ end=/)/ fold' }
+}
