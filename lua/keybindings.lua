f6e14bdb2473f7bd17a7b810efee2466c83b33a7
Timson020 <a_sa_f@126.com>
2023-01-13 11:23:04 +0800

    feat: keybinding move to lua

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,6 +2,63 @@
 local M = {}
 local map = vim.keymap.set
 
+vim.g.maplocalleader = ';'
+
+-- 默认常规快捷键 
+M.common = function ()
+	-- 保存
+	-- C-s == control + s
+	-- nmap <silent> <C-s> :w<cr>
+	map('n', '<C-s>', ':w<CR>')
+
+	-- 新开标签页
+	-- nmap <silent> <C-t> :tabnew<cr>
+	-- nmap <silent> <LocalLeader>q :tabnext<cr>
+	-- nmap <silent> <LocalLeader>e :tabprevious<cr>
+	map('n', '<C-t>', ':tabnew<CR>')
+	map('n', '<LocalLeader>q', ':tabnext<CR>')
+	map('n', '<LocalLeader>e', ':tabprevious<CR>')
+
+	-- controller + jkhl 重新控制每给窗口的大小
+	-- nmap <silent> <C-l> :vertical resize+2<cr>
+	-- nmap <silent> <C-h> :vertical resize-2<cr>
+	-- nmap <silent> <C-j> :resize+2<cr>
+	-- nmap <silent> <C-k> :resize-2<cr>
+	map('n', '<C-l>', ':vertical resize+2<CR>')
+	map('n', '<C-h>', ':vertical resize-2<CR>')
+	map('n', '<C-j>', ':resize+2<CR>')
+	map('n', '<C-k>', ':resize-2<CR>')
+
+	-- 快速浏览
+-- nmap <silent> <C-u> 4k
+-- nmap <silent> <C-d> 4j
+	map('n', '<C-u>', '4k')
+	map('n', '<C-d>', '4j')
+
+	-- 	" ;p 打开搜索文件页面
+	-- " 插件名 telescope.nvim
+	-- nmap <silent> <LocalLeader>ff :Telescope find_files<cr>
+	-- nmap <silent> <LocalLeader>fw :Telescope live_grep<cr>
+	-- nmap <silent> <LocalLeader>git :Telescope git_files<cr>
+	-- nmap <silent> <LocalLeader>buf :Telescope buffers<cr>
+	-- nmap <silent> <C-q> :Bdelete!<cr>
+	map('n', '<LocalLeader>ff', ':Telescope find_files<CR>')
+	map('n', '<LocalLeader>fw', ':Telescope live_grep<CR>')
+	map('n', '<LocalLeader>git', ':Telescope git_files<CR>')
+	map('n', '<LocalLeader>buf', ':Telescope buffers<CR>')
+	map('n', '<C-q>', ':Bdelete<CR>')
+
+	-- 	" 打开tagbar
+	-- " 插件名     simrat39/symbols-outline.nvim
+	-- nmap <silent> <LocalLeader>r :SymbolsOutline<cr>
+	map('n', '<LocalLeader>r', ':SymbolsOutline<CR>')
+
+	-- 显示文件目录
+	-- nmap <silent> <LocalLeader>te :NvimTreeToggle<cr>
+	map('n', '<LocalLeader>te', ':NvimTreeToggle<CR>')
+end
+
+--
 M.treesitter = function(mapBuf)
 	local opt = { noremap = true, silent = true }
 	-- rename
@@ -31,6 +88,7 @@ M.treesitter = function(mapBuf)
 	-- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
 end
 
+--
 M.floaterm = function ()
 	vim.g.floaterm_keymap_toggle = '<c-\\>'
 	map('n', '<LocalLeader>tn', ':FloatermToggle --autoclose=2<CR>')
@@ -38,6 +96,7 @@ M.floaterm = function ()
 	-- map('n', '<LocalLeader>tr', ':FloatermNew --height=0.99 --width=0.99 --autoclose=2 ranger<CR>')
 end
 
+--
 M.lsp = function(mapbuf)
 	local opt = { noremap = true, silent = true }
 	-- rename
@@ -66,5 +125,7 @@ M.lsp = function(mapbuf)
 	-- mapbuf('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opt)
 	-- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
 end
+
+M.common()
 
 return M
