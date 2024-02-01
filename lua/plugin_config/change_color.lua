b5b2a5b8d6cbbba5fa71bf7312ef61bff3f21f4b
Timson020 <a_sa_f@126.com>
2024-02-01 16:36:41 +0800

    feat: add colorscheme

--- lua/plugin_config/change_color.lua
+++ lua/plugin_config/change_color.lua
@@ -1,14 +1,14 @@
 local M = {
 	'chxuan/change-colorscheme',
 	config = function ()
-		-- 主题文件存储在~/.vim/color/ 目录下
-		-- 插件就可以自动读取，然后使用快捷键切换主题
 		-- 主题文件可以查看 https://github.com/flazz/vim-colorschemes.git
 		local key = require('keybindings')
 		key.color()
-		-- pcall(vim.cmd, "colorscheme " .. 'habamax')
 
-		vim.cmd("colorscheme solarized_dark")
+		local status = pcall(vim.cmd, 'colorscheme ' .. 'solarized8_dark')
+		if not status then
+			vim.cmd("colorscheme habamax")
+		end
 	end
 }
 
